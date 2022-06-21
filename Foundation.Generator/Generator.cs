85053e8d16f59b16b433476a9d9158d36e0cf1c5
Tim <hounddog@gmail.com>
2022-06-21 07:17:42 -0400

    init

---
+++ Foundation.Generator/Generator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace Foundation.Generators
+{
+    public class Generator : ISourceGenerator
+    {
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            //throw new NotImplementedException();
+        }
+
+        public void Execute(GeneratorExecutionContext context)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
