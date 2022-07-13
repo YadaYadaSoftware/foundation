53705bd004c0c91d89aa2ca833c457ffd4c6fbf1
Tim <hounddog@gmail.com>
2022-07-13 16:43:52 -0400

    Bring Back MigrationsAssemblyPath

--- Foundation.Generator/MigrationModel.cs
+++ Foundation.Generator/MigrationModel.cs
@@ -8,9 +8,6 @@ public class MigrationModel : IMigrationModel
     public TypeModel MigrationFunction { get; }
     public string MigrationMethod { get; }
     public string DependsOn { get; }
-    public string SqlScriptsBucket { get; }
-    public string Branch { get; }
-    public string MigrationsAssembly { get; set; }
     public string MigrationsAssemblyPath { get; set; }
 
     public MigrationModel(string migrationId, TypeModel dataMigrationFunction, string migrationMethod, string dependsOn, string sqlScriptsBucket, string branch, string migrationAssembly, string migrationsAssemblyPath)
@@ -21,10 +18,7 @@ public class MigrationModel : IMigrationModel
         MigrationFunction = dataMigrationFunction ?? throw new ArgumentNullException(nameof(dataMigrationFunction));
         MigrationMethod = migrationMethod;
         DependsOn = dependsOn;
-        SqlScriptsBucket = sqlScriptsBucket;
-        Branch = branch;
         // ATTRIBUTE:  ADD HERE
-        MigrationsAssembly = migrationAssembly;
         MigrationsAssemblyPath = migrationsAssemblyPath;
     }
 }
