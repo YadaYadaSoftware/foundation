8661687b7f68ff2134f7b0a43572ac08c20f405b
Tim <hounddog@gmail.com>
2022-07-13 18:17:27 -0400

    clean up dead parameters

--- Foundation.Generator/MigrationModel.cs
+++ Foundation.Generator/MigrationModel.cs
@@ -10,7 +10,7 @@ public class MigrationModel : IMigrationModel
     public string DependsOn { get; }
     public string MigrationsAssemblyPath { get; set; }
 
-    public MigrationModel(string migrationId, TypeModel dataMigrationFunction, string migrationMethod, string dependsOn, string sqlScriptsBucket, string branch, string migrationAssembly, string migrationsAssemblyPath)
+    public MigrationModel(string migrationId, TypeModel dataMigrationFunction, string migrationMethod, string dependsOn, string migrationsAssemblyPath)
     {
         if (string.IsNullOrEmpty(migrationMethod)) throw new ArgumentException("Value cannot be null or empty.", nameof(migrationMethod));
         if (string.IsNullOrEmpty(migrationId)) throw new ArgumentException("Value cannot be null or empty.", nameof(migrationId));
