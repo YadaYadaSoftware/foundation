0a39e6c6c0c938b907e89795913c5d98ff4ee6a9
Tim <hounddog@gmail.com>
2022-08-11 07:48:13 -0400

    Revert "DatabaseSerializationContext"
    This reverts commit ed0bb248ffcd844b964220e1bbfbf86f2f7b1567.

--- Foundation.Functions/Startup.cs
+++ Foundation.Functions/Startup.cs
@@ -1,54 +1,43 @@
-using System.Text.Json;
-using System.Text.Json.Serialization;
-using System.Text.Json.Serialization.Metadata;
 using Amazon.Lambda.Core;
 using Amazon.S3.Transfer;
-using Data.Serverless.Backup;
-using Data.Serverless.Migrate;
-using Foundation.Functions;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 
 
-[assembly: LambdaSerializer(typeof(DatabaseSerializationContext))]
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
-namespace Foundation.Functions;
-
-[JsonSerializable(typeof(BackupRestoreDatabaseInfo))]
-[JsonSerializable(typeof(MigrationInfoRequest))]
-public partial class DatabaseSerializationContext : System.Text.Json.Serialization.JsonSerializerContext
+namespace Foundation.Functions
 {
-}
-
-[Amazon.Lambda.Annotations.LambdaStartup]
-public class Startup
-{
-    /// <summary>
-    /// Services for Lambda functions can be registered in the services dependency injection container in this method. 
-    ///
-    /// The services can be injected into the Lambda function through the containing type's constructor or as a
-    /// parameter in the Lambda function using the FromService attribute. Services injected for the constructor have
-    /// the lifetime of the Lambda compute container. Services injected as parameters are created within the scope
-    /// of the function invocation.
-    /// </summary>
-    public void ConfigureServices(IServiceCollection services)
+    [Amazon.Lambda.Annotations.LambdaStartup]
+    public class Startup
     {
-        var builder = new ConfigurationBuilder();
+        /// <summary>
+        /// Services for Lambda functions can be registered in the services dependency injection container in this method. 
+        ///
+        /// The services can be injected into the Lambda function through the containing type's constructor or as a
+        /// parameter in the Lambda function using the FromService attribute. Services injected for the constructor have
+        /// the lifetime of the Lambda compute container. Services injected as parameters are created within the scope
+        /// of the function invocation.
+        /// </summary>
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = new ConfigurationBuilder();
 
-        builder.AddJsonFile("appsettings.json", true, false)
-            .AddSystemsManager("/db.deploy2the.cloud");
+            builder.AddJsonFile("appsettings.json", true, false)
+                .AddSystemsManager("/db.deploy2the.cloud");
 
-        var configurationRoot = builder.Build();
+            var configurationRoot = builder.Build();
 
-        services.Configure<SqlConnectionStringBuilder>(configurationRoot.GetSection(nameof(SqlConnectionStringBuilder)))
-            .AddLogging(loggingBuilder => loggingBuilder.AddLoggerYadaYada(configurationRoot, nameof(LambdaLoggerOptions)))
-            //.AddEntityFrameworkSqlServer()
-            //.AddEntityConfigurations()
-            .AddAWSService<Amazon.S3.IAmazonS3>()
-            .AddSingleton<ITransferUtility, TransferUtility>()
-            //.AddDbContext<DbContext, DbContext>((provider, optionsBuilder) => optionsBuilder.UseSqlServer(provider.GetRequiredService<IOptions<SqlConnectionStringBuilder>>().Value.ConnectionString))
-            ;
+            services.Configure<SqlConnectionStringBuilder>(configurationRoot.GetSection(nameof(SqlConnectionStringBuilder)))
+                .AddLogging(loggingBuilder => loggingBuilder.AddLoggerYadaYada(configurationRoot, nameof(LambdaLoggerOptions)))
+                //.AddEntityFrameworkSqlServer()
+                //.AddEntityConfigurations()
+                .AddAWSService<Amazon.S3.IAmazonS3>()
+                .AddSingleton<ITransferUtility, TransferUtility>()
+                //.AddDbContext<DbContext, DbContext>((provider, optionsBuilder) => optionsBuilder.UseSqlServer(provider.GetRequiredService<IOptions<SqlConnectionStringBuilder>>().Value.ConnectionString))
+                ;
+        }
     }
 }
